86d8b0dd5c4b4faea3d4d3f703ca69c6992cf016
Akira Sasaki <gclue.akira@gmail.com>
2020-06-12 11:49:00 +0900

    Deployed 8cb7f12 with MkDocs version: 1.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -4,15 +4,14 @@ window.onload = function() {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
         	src = src.match(".+/(.+?)$")[1];
-        	var out = '';
-        	out += '<div class="youtube">';
-        	out += '<iframe width="853" height="480" ';
-        	out += 'src="https://www.youtube-nocookie.com/embed/';
+        	var out = "";
+        	out += "<div class=\"youtube\">";
+        	out += "<iframe width=\"853\" height=\"480\" ";
+        	out += "src=\"https://www.youtube-nocookie.com/embed/";
         	out += src;
-        	out += ' ';
-        	out += 'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>';
-            out += '</iframe>';
-        	out += '</div>';
+        	out += "\" ";
+        	out += "frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+        	out += "</div>";
         	tag[i].outerHTML = out;
         }
     }
