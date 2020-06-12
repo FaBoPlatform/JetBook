bc04418bfb95cefb4b2beecbba04e0d83cdd2166
Akira Sasaki <gclue.akira@gmail.com>
2020-06-12 10:22:56 +0900

    Deployed 8cb7f12 with MkDocs version: 1.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var tag = document.getElementsByTagName('img');
-    for (var i = 0; i < tag.length; i++) {
+    for (var i = 0; i <= tag.length; i++) {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
         	src = src.match(".+/(.+?)$")[1];
