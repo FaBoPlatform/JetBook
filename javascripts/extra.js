df428a59e0a735276c889c79772db979265d4213
Akira Sasaki <gclue.akira@gmail.com>
2020-06-12 11:28:59 +0900

    Deployed 8cb7f12 with MkDocs version: 1.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -7,7 +7,7 @@ window.onload = function() {
         	var out = "";
         	out += "<div class=\"youtube\">";
         	out += "<iframe width=\"853\" height=\"480\" ";
-        	out += "src=\"https://www.youtube.com/embed/";
+        	out += "src=\"https://www.youtube-nocookie.com/embed/";
         	out += src;
         	out += "\" ";
         	out += "frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
