21f7bf67996a50c7dcb7f2c7fa730f56a8e8af81
Akira Sasaki <gclue.akira@gmail.com>
2020-06-12 11:39:18 +0900

    Deployed 8cb7f12 with MkDocs version: 1.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -4,14 +4,15 @@ window.onload = function() {
         if(tag[i].alt == "YOUTUBE") {
         	var src = tag[i].src;
         	src = src.match(".+/(.+?)$")[1];
-        	var out = "";
-        	out += "<div class=\"youtube\">";
-        	out += "<iframe width=\"853\" height=\"480\" ";
-        	out += "src=\"https://www.youtube-nocookie.com/embed/";
+        	var out = '';
+        	out += '<div class="youtube">';
+        	out += '<iframe width="853" height="480" ';
+        	out += 'src="https://www.youtube-nocookie.com/embed/';
         	out += src;
-        	out += "\" ";
-        	out += "frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
-        	out += "</div>";
+        	out += ' ';
+        	out += 'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>';
+            out += '</iframe>';
+        	out += '</div>';
             console.log(tag[i].outerHTML)
         	//tag[i].outerHTML = out;
         }
